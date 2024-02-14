5be3ef801f5146ce72a007e657cd57d613f408cd
hamza <hamzabouziane941@gmail.com>
2024-02-14 13:13:07 +0100

    init spring boot project + implement github oauth2

---
+++ src/main/java/com/oauth/project/OAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.oauth.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthProjectApplication.class, args);
+	}
+
+}
