ad25c9434b1c4df1345bad66bd568987087f46a5
hamza <hamzabouziane941@gmail.com>
2024-02-14 13:13:07 +0100

    init spring boot project + implement github oauth2

---
+++ src/main/java/com/oauth/project/web/MyResource.java
@@ -0,0 +1,15 @@
+package com.oauth.project.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyResource {
+
+  private String name;
+}
